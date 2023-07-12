3db5c9b527b8a7c016c42921ed9299214880316d
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2023-07-12 12:47:05 -0400

    set fake return value for uptime

--- tests/basic_application_code/src/main.c
+++ tests/basic_application_code/src/main.c
@@ -5,9 +5,11 @@
 
 DEFINE_FFF_GLOBALS;
 FAKE_VOID_FUNC(z_log_minimal_printk);
+FAKE_VALUE_FUNC(int64_t, k_uptime_get);
 
 ZTEST(application_unit_tests, test_mytest)
 {
+  k_uptime_get_fake.return_val = 200;
   int result = add_200();
   zassert_equal(result, 200, "Expected 200, got: %d", result);
 }
